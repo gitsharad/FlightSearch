8f7efba450954603f5da3c8364e4041ee2d94f30
sharad <sharad.pawar@pb.com>
2018-07-02 09:46:54 +0530

    third commit.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,18 +3,27 @@ angular.module('flightController', [])
 	// inject the Todo service factory into our controller
 	.controller('mainController', ['$scope', '$http', "Flight", function ($scope, $http, Flight) {
   
-	        var _this = this;
+			var _this = this;
+			_this.showHide = true;
 			    _this.labelArray = {
 					    "Pune":"PNQ",
 						"Delhi":"DEL",
 						"Banglore":"BAG",
 						"Chandigarh":"CHG"
 				}
+		   _this.returnbtn = true;
+		   _this.waybtnDSB = function(){
+			  _this.waybtn = true;
+			  _this.returnbtn = false;
+		   }
+		   _this.returnbtnDSB = function(){
+			_this.returnbtn = true;
+			_this.waybtn = false;
+		 }
 	
         Flight.getFlightData().then(
 		function(values){
 		   _this.flights = values.data.flightData;
-			//console.log("values",values.data.flightData[0]);
 		   _this.origin = values.data.flightData[0].origin;
 		   _this.destination = values.data.flightData[0].destination;
 		});
@@ -22,22 +31,26 @@ angular.module('flightController', [])
 		$scope.searchFlight = function (user) {
 			var availableFlights = [];
 			_.each( _this.flights, function (f) {
-				var orig = f.origin.toUpperCase();
-				var dest = f.destination.toUpperCase();
+				_this.origin = f.origin.toUpperCase();
+				_this.destination  = f.destination.toUpperCase();
 				// console.log("dd",user.originCity.toUpperCase() ,orig, user.destinationCity.toUpperCase() ,dest);
-				if ((user.originCity.toUpperCase() == orig) && (user.destinationCity.toUpperCase() == dest)) {
+				if ((user.originCity.toUpperCase() == _this.origin) && (user.destinationCity.toUpperCase() == _this.destination)) {
 					_.each(f.flights, function (fl) {
-						console.log("dddemo",user.depdate ,fl.departure_date, user.retdate ,fl.return_date);
-						
-						if ((user.depdate == fl.departure_date) && (user.retdate == fl.return_date)) {
+					  if ((user.depdate == fl.departure_date) && (user.retdate == fl.return_date)) {
 							availableFlights.push(fl);
 						}
-					})
-				}
+					  })
+			}
 			})
-			console.log("data", _this.flights);
+
+			if(!availableFlights.length){
+				_this.origin = "";
+				_this.destination  = "";
+				_this.showHide = false;
+				
+			}
 			_this.flights = [{"flights":availableFlights}];
-			console.log("data2", _this.flights);
+			// console.log("data2", _this.flights);
 		}
 
 
